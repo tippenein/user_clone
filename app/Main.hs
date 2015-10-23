824da9811561fde878ce0d974a4602ff5f49d2fd
brady.ouren <brady.ouren@gmail.com>
2015-10-23 00:43:06 -0700

    fix parseJSON instance

--- app/Main.hs
+++ app/Main.hs
@@ -1,53 +1,54 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE TypeOperators     #-}
 
 module Main where
 
-import           Control.Monad.Trans.Either
-import           Data.Aeson
-import           Data.Aeson.TH              (defaultOptions, deriveJSON)
-import           Data.List
-import           Data.Text
-import           Data.Time
-import           GHC.Generics
-import           Network.Wai
-import           Network.Wai.Handler.Warp
-import           Servant
+import Control.Monad
+import Control.Monad.Trans.Either
+import Data.Aeson
+import Data.Aeson.TH              (defaultOptions, deriveJSON)
+import Data.List
+import Data.Text
+import Data.Text.Internal
+import Data.Time
+import GHC.Generics
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
 
-$(deriveJSON defaultOptions ''Day)
 
 data User = User {
-    firstName :: String
-  , lastName  :: String
+    firstName :: Text
+  , lastName  :: Text
   , ssn       :: SSN
   , email     :: Email
   , address   :: Address
-  , phone     :: String
+  , phone     :: Text
   , dob       :: Day
   } deriving (Show, Generic)
 
 data Address = Address {
-    street    :: String
-  , city      :: String
-  , state     :: String
-  , aptNumber :: String
-  , zipCode   :: String
+    street    :: Text
+  , city      :: Text
+  , state     :: Text
+  , aptNumber :: Text
+  , zipCode   :: Text
   } deriving (Show, Generic)
 
 instance ToJSON User
 instance FromJSON User
 
--- instance FromJSON Day where
---   parseJSON (String d) = readTime defaultTimeLocale "%Y-%m-%d" d
---   parseJSON _ = fail "failed to parse date"
+instance ToJSON Day where
+  toJSON = toJSON . showGregorian
 
--- instance ToJSON Day where
---   -- display a day in YYYY-mm-dd format
---   toJSON d = toJSON $ showGregorian d
+parseDate :: String -> Day
+parseDate = parseTimeOrError True defaultTimeLocale "%Y-%m-%d"
+
+instance FromJSON Day where
+  parseJSON (Object v) = liftM parseDate (v .: "date")
 
 instance ToJSON Email
 instance FromJSON Email
@@ -56,8 +57,8 @@ instance FromJSON SSN
 instance ToJSON Address
 instance FromJSON Address
 
-newtype SSN = SSN String deriving (Show, Generic)
-newtype Email = Email String deriving (Show, Generic)
+newtype SSN = SSN Text deriving (Show, Generic)
+newtype Email = Email Text deriving (Show, Generic)
 
 type UserAPI =
     -- GET /users
